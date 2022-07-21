256a32621bb5c1a870ffcb63c0aa3cec73e1dc9e
amltms <amalthomas587@gmail.com>
2022-07-21 13:25:13 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Films and TV shows review app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
+
+export default Home
